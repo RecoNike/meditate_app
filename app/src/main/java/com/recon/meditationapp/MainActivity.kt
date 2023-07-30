c8b1cc23f9cf778f96a59d70d4bee8918c330dd8
RecoNike <reconike@gmail.com>
2023-07-30 15:15:47 +0300

    Initialization Commit

---
+++ app/src/main/java/com/recon/meditationapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.recon.meditationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
